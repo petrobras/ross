3dd5aa3eca6161ae28f2c31d59c1e067219716a6
rodrigomoliveira1 <45969994+rodrigomoliveira1@users.noreply.github.com>
2019-01-16 15:43:35 -0200

    Removed unused imports
    Removed some imports that are not being used for now

--- disk_element.py
+++ disk_element.py
@@ -1,15 +1,7 @@
-import warnings
 import numpy as np
 import pandas as pd
 import scipy.interpolate as interpolate
-import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-from ross.data_io.read_xl import (
-    load_bearing_seals_from_yaml,
-    load_bearing_seals_from_xltrc,
-    load_disks_from_xltrc,
-    load_shaft_from_xltrc,
-)
 
 __all__ = [
     "LumpedDiskElement",
