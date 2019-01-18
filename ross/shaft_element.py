14e8f8b2b701d02012c5228baabd522d6897253f
rodrigomoliveira1 <45969994+rodrigomoliveira1@users.noreply.github.com>
2019-01-18 11:45:01 -0200

    Fixed the import issue

--- ross/shaft_element.py
+++ ross/shaft_element.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.patches as mpatches
 from abc import ABC
-from ross.ross.element.py import *
+from element import Element
 
 __all__ = [
     "ShaftElement"
