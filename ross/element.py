6d03dedd5a2cb0b24114fb63c8a8dc00984e7238
raphaeltimbo <raphaelts@gmail.com>
2019-01-21 17:03:06 -0200

    Remove summary
    This function will not be needed since we do not want to use pandas for
    now.

--- ross/element.py
+++ ross/element.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from abc import ABC, abstractmethod
 
 
@@ -11,15 +10,7 @@ class Element(ABC):
     def __init__(self):
         pass
 
-    def summary(self):
-        """A summary for the element.
-        A pandas series with the element properties as variables.
-        """
-        attributes = self.__dict__
-        attributes["type"] = self.__class__.__name__
-        return pd.Series(attributes)
-    
-    @abstractmethod    
+    @abstractmethod
     def M(self):
         pass
     
