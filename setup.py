3540c09209ee452addfff7b79eb20e085ce50438
raphaeltimbo <raphaelts@gmail.com>
2019-01-21 17:14:39 -0200

    Integrate setup.py with pytest
    When we type:
    python setup.py test
    this will execute the tests using pytest-runner.

--- setup.py
+++ setup.py
@@ -112,6 +112,8 @@ setup(
     # },
     install_requires=REQUIRED,
     extras_require=EXTRAS,
+    setup_requires=['pytest-runner'],
+    tests_requires=['pytest'],
     include_package_data=True,
     license='MIT',
     classifiers=[
