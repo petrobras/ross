6917ee9fa8d91f3ab3ff47d7f5e6abfead643638
Gabriel Bachmann <GabrielBachmann@poli.ufrj.br>
2019-01-25 11:03:46 -0200

    test implementation for the Material class.

---
+++ ross/tests/test_materials.py
@@ -0,0 +1,78 @@
+import pytest
+from ross.materials import *
+from numpy.testing import assert_allclose
+
+
+AISI4140 = Material.use_material('AISI4140')
+
+
+def test_raise_name_material():
+    with pytest.raises(AssertionError) as excinfo:
+        mat = Material('with space', rho=7850, G_s=80e9, Poisson=0.27)
+    assert 'Spaces are not allowed' in str(excinfo.value)
+
+
+def test_E():
+    mat = Material(name='test', rho=7850, G_s=80e9, Poisson=0.27)
+    assert_allclose(mat.E, 203.2e9)
+    assert_allclose(mat.G_s, 80e9)
+    assert_allclose(mat.Poisson, 0.27)
+
+
+def test_G_s():
+    mat = Material(name='test', rho=7850, E=203.2e9, Poisson=0.27)
+    assert_allclose(mat.E, 203.2e9)
+    assert_allclose(mat.G_s, 80e9)
+    assert_allclose(mat.Poisson, 0.27)
+
+
+def test_Poisson():
+    mat = Material(name='test', rho=7850, E=203.2e9, G_s=80e9)
+    assert_allclose(mat.E, 203.2e9)
+    assert_allclose(mat.G_s, 80e9)
+    assert_allclose(mat.Poisson, 0.27)
+
+
+def test_E_G_s_Poisson():
+    mat = Material(name='test', rho=7850, E=203.2e9, G_s=80e9, Poisson=0.27)
+    assert_allclose(mat.E, 203.2e9)
+    assert_allclose(mat.G_s, 80e9)
+    assert_allclose(mat.Poisson, 0.27)
+
+
+def test_specific_material():
+    assert_allclose(AISI4140.rho, 7850)
+    assert_allclose(AISI4140.E, 203.2e9)
+    assert_allclose(AISI4140.G_s, 80e9)
+    assert_allclose(AISI4140.Poisson, 0.27)
+
+
+def test_error_rho():
+    with pytest.raises(TypeError) as ex:
+        Material(name='test', E=203.2e9, G_s=80e9)
+
+    assert "__init__() missing 1 required positional argument: 'rho'" == str(ex.value)
+
+
+def test_error_E_G_s_Poisson():
+    with pytest.raises(AssertionError) as ex:
+        Material(name='test', rho=785, E=203.2e9)
+    assert 'At least 2 arguments from E' in str(ex.value)
+
+# Serialization tests.
+
+
+def test_available_materials():
+    Material.available_materials()
+
+
+def test_serialization():
+    available = Material.available_materials()
+    obj1 = Material(name='obj1', rho =92e1, E=281.21, G_s=20e9)
+    obj1.save_material()
+    obj2 = Material.use_material('obj1')
+    assert obj1.__dict__ == obj2.__dict__
+
+    obj1.remove_material('obj1')
+    available_after = Material.available_materials()
+    assert available == available_after
