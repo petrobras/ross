8e795c9470d8fc9d2bfa77e48acf16081295cf29
raphaeltimbo <raphaelts@gmail.com>
2019-04-09 17:28:16 -0300

    Remove imports from __init__ to simplify interface
    This removes imports such as rst and bse, since the user will be able
    to access those with rs.bearing_seal_element, and if the user wants to
    use the BearingElement class for example, it can be done with
    rs.BearingElement, since there is already a line with
    'from .bearing_seal_element import *'.
    For tests this changes from ross import * to import ross as rs.
    Although it is not considered a bad practice to use * on the test files
    we should try to replicate on our tests how the user will call the
    library.

--- ross/__init__.py
+++ ross/__init__.py
@@ -1,8 +1,3 @@
-from . import bearing_seal_element as bse
-from . import disk_element as disk
-from . import results as rst
-from . import rotor_assembly as rotor
-from . import shaft_element as shaft
 from .materials import *
 from .bearing_seal_element import *
 from .disk_element import *
