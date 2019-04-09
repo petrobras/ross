e57b2fbac016f477a9f8f4d5e4d0966a1ea9edad
raphaeltimbo <raphaelts@gmail.com>
2019-04-09 16:24:32 -0300

    Remove unused import

--- ross/tests/test_disk_element.py
+++ ross/tests/test_disk_element.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from numpy.testing import assert_almost_equal, assert_allclose
+from numpy.testing import assert_almost_equal
 from ross.disk_element import DiskElement
 from ross.materials import steel
 
