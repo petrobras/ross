76b91601efc3b5748c5da5b6e6bff58869ff81c8
flaviorangel <flm.rangel@gmail.com>
2019-05-24 20:18:31 -0300

    Update tests for optional eccentricity and load

--- ross/tests/test_fluid_flow.py
+++ ross/tests/test_fluid_flow.py
@@ -2,7 +2,7 @@ from ross.fluid_flow import fluid_flow as flow
 import math
 
 
-def fluid_flow0(eccentricity_ratio):
+def fluid_flow_eccentricity():
     nz = 30
     ntheta = 20
     nradius = 11
@@ -12,12 +12,30 @@ def fluid_flow0(eccentricity_ratio):
     p_out = 0.
     radius_rotor = 0.0499
     radius_stator = 0.05
-    eccentricity = (radius_stator - radius_rotor)*eccentricity_ratio
+    eccentricity = (radius_stator - radius_rotor)*0.2663
     visc = 0.1
     rho = 860.
     return flow.PressureMatrix(nz, ntheta, nradius, length, omega, p_in,
                                p_out, radius_rotor, radius_stator,
-                               eccentricity, visc, rho)
+                               visc, rho, eccentricity=eccentricity)
+
+
+def fluid_flow_load():
+    nz = 30
+    ntheta = 20
+    nradius = 11
+    length = 0.03
+    omega = 157.1
+    p_in = 0.
+    p_out = 0.
+    radius_rotor = 0.0499
+    radius_stator = 0.05
+    load = 525
+    visc = 0.1
+    rho = 860.
+    return flow.PressureMatrix(nz, ntheta, nradius, length, omega, p_in,
+                               p_out, radius_rotor, radius_stator,
+                               visc, rho, load=load)
 
 
 def test_sommerfeld_number():
@@ -26,9 +44,8 @@ def test_sommerfeld_number():
     expected results for the sommerfeld number and eccentricity ratio.
     Taken from example 5.5.1, page 181 (Dynamics of rotating machine, FRISSWELL)
     """
-    bearing = fluid_flow0(0)
-    e = bearing.calculate_eccentricity_ratio(525)
-    assert math.isclose(e, 0.2663, rel_tol=0.001)
+    bearing = fluid_flow_load()
+    assert math.isclose(bearing.eccentricity_ratio, 0.2663, rel_tol=0.001)
 
 
 def test_get_rotor_load():
@@ -37,9 +54,8 @@ def test_get_rotor_load():
     expected results for the load over the rotor, given the eccentricity ratio.
     Taken from example 5.5.1, page 181 (Dynamics of rotating machine, FRISSWELL)
     """
-    bearing = fluid_flow0(0.2663)
-    f = bearing.get_rotor_load()
-    assert math.isclose(f, 525, rel_tol=0.1)
+    bearing = fluid_flow_eccentricity()
+    assert math.isclose(bearing.load, 525, rel_tol=0.1)
 
 
 def test_stiffness_matrix():
@@ -48,7 +64,7 @@ def test_stiffness_matrix():
     expected results for the stiffness matrix, given the eccentricity ratio.
     Taken from example 5.5.1, page 181 (Dynamics of rotating machine, FRISSWELL)
     """
-    bearing = fluid_flow0(0.2663)
+    bearing = fluid_flow_eccentricity()
     kxx, kxy, kyx, kyy = bearing.get_analytical_stiffness_matrix()
     assert math.isclose(kxx/10**6, 12.81, rel_tol=0.01)
     assert math.isclose(kxy/10**6, 16.39, rel_tol=0.01)
@@ -62,7 +78,7 @@ def test_damping_matrix():
     expected results for the damping matrix, given the eccentricity ratio.
     Taken from example 5.5.1, page 181 (Dynamics of rotating machine, FRISSWELL)
     """
-    bearing = fluid_flow0(0.2663)
+    bearing = fluid_flow_load()
     cxx, cxy, cyx, cyy = bearing.get_analytical_damping_matrix()
     assert math.isclose(cxx/10**3, 232.9, rel_tol=0.01)
     assert math.isclose(cxy/10**3, -81.92, rel_tol=0.01)
