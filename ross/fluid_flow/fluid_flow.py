a74d20c6ccbf2f0873e54a5aa08a6e7bac329e6e
flaviorangel <flm.rangel@gmail.com>
2019-06-10 14:41:36 -0300

    Change doctest

--- ross/fluid_flow/fluid_flow.py
+++ ross/fluid_flow/fluid_flow.py
@@ -148,12 +148,11 @@ class PressureMatrix:
     >>> my_fluid_flow.calculate_pressure_matrix_analytical() # doctest: +ELLIPSIS
     array([[-0.00000...
     >>> my_fluid_flow.calculate_pressure_matrix_numerical() # doctest: +ELLIPSIS
-    array([[-3...
+    array([[...
     >>> show(my_fluid_flow.plot_eccentricity())
     >>> show(my_fluid_flow.plot_pressure_theta(z=int(nz/2)))
     >>> my_fluid_flow.matplot_pressure_theta(z=int(nz/2)) # doctest: +ELLIPSIS
     <matplotlib.axes._subplots.AxesSubplot object at...
-    >>> plt.show()
     """
 
     def __init__(
