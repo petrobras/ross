40deb72fd746c97c271b85eae9ea7ab89173c600
raphaeltimbo <raphaelts@gmail.com>
2019-08-07 17:43:47 -0300

    Fix code coverage
    Code coverage is broken due to commit 97956c9 which removed the codecov
    from the travisci installation packages.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ REQUIRED = [
 ]
 
 # What packages are optional?
-EXTRAS = {"dev": ["pytest", "codecov"]}
+EXTRAS = {"dev": ["pytest", "pytest-cov", "codecov"]}
 
 # The rest you shouldn't have to touch too much :)
 # ------------------------------------------------
