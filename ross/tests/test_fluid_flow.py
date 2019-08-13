15e5d44a4771908c3817b54e12a6ec39c93c2b14
flaviorangel <flm.rangel@gmail.com>
2019-08-13 16:45:46 -0300

    Add test for numerical long bearing

--- ross/tests/test_fluid_flow.py
+++ ross/tests/test_fluid_flow.py
@@ -6,7 +6,7 @@ from bokeh.plotting import figure
 import matplotlib.pyplot as plt
 
 
-def fluid_flow_eccentricity():
+def fluid_flow_short_eccentricity():
     nz = 30
     ntheta = 20
     nradius = 11
@@ -24,7 +24,7 @@ def fluid_flow_eccentricity():
                                visc, rho, eccentricity=eccentricity)
 
 
-def fluid_flow_load():
+def fluid_flow_short_load():
     nz = 30
     ntheta = 20
     nradius = 11
@@ -48,7 +48,7 @@ def test_sommerfeld_number():
     expected results for the sommerfeld number and eccentricity ratio.
     Taken from example 5.5.1, page 181 (Dynamics of rotating machine, FRISSWELL)
     """
-    bearing = fluid_flow_load()
+    bearing = fluid_flow_short_load()
     assert math.isclose(bearing.eccentricity_ratio, 0.2663, rel_tol=0.001)
 
 
@@ -58,7 +58,7 @@ def test_get_rotor_load():
     expected results for the load over the rotor, given the eccentricity ratio.
     Taken from example 5.5.1, page 181 (Dynamics of rotating machine, FRISSWELL)
     """
-    bearing = fluid_flow_eccentricity()
+    bearing = fluid_flow_short_eccentricity()
     assert math.isclose(bearing.load, 525, rel_tol=0.1)
 
 
@@ -68,7 +68,7 @@ def test_stiffness_matrix():
     expected results for the stiffness matrix, given the eccentricity ratio.
     Taken from example 5.5.1, page 181 (Dynamics of rotating machine, FRISSWELL)
     """
-    bearing = fluid_flow_eccentricity()
+    bearing = fluid_flow_short_eccentricity()
     kxx, kxy, kyx, kyy = bearing.get_analytical_stiffness_matrix()
     assert math.isclose(kxx/10**6, 12.81, rel_tol=0.01)
     assert math.isclose(kxy/10**6, 16.39, rel_tol=0.01)
@@ -82,7 +82,7 @@ def test_damping_matrix():
     expected results for the damping matrix, given the eccentricity ratio.
     Taken from example 5.5.1, page 181 (Dynamics of rotating machine, FRISSWELL)
     """
-    bearing = fluid_flow_load()
+    bearing = fluid_flow_short_load()
     cxx, cxy, cyx, cyy = bearing.get_analytical_damping_matrix()
     assert math.isclose(cxx/10**3, 232.9, rel_tol=0.01)
     assert math.isclose(cxy/10**3, -81.92, rel_tol=0.01)
@@ -90,7 +90,7 @@ def test_damping_matrix():
     assert math.isclose(cyy/10**3, 294.9, rel_tol=0.01)
 
 
-def fluid_flow_numerical():
+def fluid_flow_short_numerical():
     nz = 8
     ntheta = 64
     nradius = 11
@@ -109,8 +109,28 @@ def fluid_flow_numerical():
                                radius_stator, visc, rho, beta=beta, eccentricity=eccentricity)
 
 
+def fluid_flow_long_numerical():
+    nz = 8
+    ntheta = 132
+    nradius = 11
+    omega = 100. * 2 * np.pi / 60
+    p_in = 0.
+    p_out = 0.
+    radius_rotor = 1
+    h = 0.000194564
+    radius_stator = radius_rotor + h
+    length = 8 * radius_stator
+    visc = 0.015
+    rho = 860.
+    beta = np.pi
+    eccentricity = 0.0001
+    return flow.PressureMatrix(nz, ntheta, nradius, length,
+                               omega, p_in, p_out, radius_rotor,
+                               radius_stator, visc, rho, beta=beta, eccentricity=eccentricity)
+
+
 def test_numerical_abs_error():
-    bearing = fluid_flow_numerical()
+    bearing = fluid_flow_short_numerical()
     bearing.calculate_pressure_matrix_analytical()
     bearing.calculate_pressure_matrix_numerical()
     error = np.linalg.norm(bearing.p_mat_analytical[:][int(bearing.nz / 2)] -
@@ -119,7 +139,7 @@ def test_numerical_abs_error():
 
 
 def test_numerical_abs_error2():
-    bearing = fluid_flow_numerical()
+    bearing = fluid_flow_short_numerical()
     bearing.calculate_pressure_matrix_analytical(method=1)
     bearing.calculate_pressure_matrix_numerical()
     error = np.linalg.norm(bearing.p_mat_analytical[:][int(bearing.nz / 2)] -
@@ -127,8 +147,17 @@ def test_numerical_abs_error2():
     assert math.isclose(error, 0, abs_tol=0.001)
 
 
+def test_long_bearing():
+    bearing = fluid_flow_long_numerical()
+    bearing.calculate_pressure_matrix_analytical()
+    bearing.calculate_pressure_matrix_numerical()
+    error = (max(bearing.p_mat_analytical[int(bearing.nz / 2)]) -
+             max(bearing.p_mat_numerical[int(bearing.nz / 2)])) / max(bearing.p_mat_numerical[int(bearing.nz / 2)])
+    assert math.isclose(error, 0, abs_tol=0.02)
+
+
 def test_bokeh_plots():
-    bearing = fluid_flow_numerical()
+    bearing = fluid_flow_short_numerical()
     bearing.calculate_pressure_matrix_numerical()
     figure_type = type(figure())
     assert isinstance(bearing.plot_shape(), figure_type)
@@ -138,7 +167,7 @@ def test_bokeh_plots():
 
 
 def test_matplotlib_plots():
-    bearing = fluid_flow_numerical()
+    bearing = fluid_flow_short_numerical()
     bearing.calculate_pressure_matrix_analytical()
     ax_type = type(plt.gca())
     assert isinstance(bearing.matplot_eccentricity(), ax_type)
