df0d1f26046181bcdf72cbda7b045bf8ce4f8cb0
flaviorangel <flm.rangel@gmail.com>
2019-09-27 13:01:00 -0300

    Add missing argument to plot function in main docstring

--- ross/fluid_flow/fluid_flow.py
+++ ross/fluid_flow/fluid_flow.py
@@ -151,7 +151,7 @@ class PressureMatrix:
     >>> # to show the plots you can use:
     >>> # show(my_fluid_flow.plot_eccentricity())
     >>> # show(my_fluid_flow.plot_pressure_theta(z=int(nz/2)))
-    >>> matplot_pressure_theta(z=int(nz/2)) # doctest: +ELLIPSIS
+    >>> matplot_pressure_theta(my_fluid_flow, z=int(nz/2)) # doctest: +ELLIPSIS
     <matplotlib.axes._subplots.AxesSubplot object at...
     """
 
