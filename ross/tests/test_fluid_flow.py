a2a07cb737ebee4624f854de4b702419cd44f709
flaviorangel <flm.rangel@gmail.com>
2019-11-26 17:04:50 -0200

    Modify equilibrium_position function test

--- ross/tests/test_fluid_flow.py
+++ ross/tests/test_fluid_flow.py
@@ -317,8 +317,9 @@ def test_matplotlib_plots():
 
 
 def test_find_equilibrium_position():
-    bearing = fluid_flow_short_friswell()
-    bearing = find_equilibrium_position(bearing, print_along=False, tolerance=0.1, increment_factor=1e-02)
+    bearing = flow.fluid_flow_example2()
+    bearing = find_equilibrium_position(bearing, print_along=False, tolerance=0.1, increment_factor=0.01,
+                                        max_iterations=5, increment_reduction_limit=1e-03)
     assert_allclose(bearing.eccentricity, (bearing.radius_stator - bearing.radius_rotor)*0.2663, atol=0.001)
 
 
