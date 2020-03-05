7546bb2f37907e978aa434a2afce3d929b7a7ca6
raphaeltimbo <raphaelts@gmail.com>
2020-03-05 17:43:40 -0300

    Change fluid_flow_test to avoid gca()
    When running the tests (fluid_flow_graphics.py + test_fluid_flow.py)
    our code can sometimes run the function plt.gca(), which is supposed
    to get the current matplotlib axis in case the user does not provide
    an axes. The problem with this is that, when testing, a function
    could get a matplotlib axes with a polar projection (resulting from
    another test) and this could result in error on our function.

--- ross/tests/test_fluid_flow.py
+++ ross/tests/test_fluid_flow.py
@@ -52,7 +52,7 @@ def fluid_flow_short_eccentricity():
         visc,
         rho,
         eccentricity=eccentricity,
-        immediately_calculate_pressure_matrix_numerically=False
+        immediately_calculate_pressure_matrix_numerically=False,
     )
 
 
@@ -84,7 +84,7 @@ def fluid_flow_short_friswell(set_load=True):
             visc,
             rho,
             load=load,
-            immediately_calculate_pressure_matrix_numerically=False
+            immediately_calculate_pressure_matrix_numerically=False,
         )
     else:
         return flow.FluidFlow(
@@ -100,7 +100,7 @@ def fluid_flow_short_friswell(set_load=True):
             visc,
             rho,
             eccentricity=eccentricity,
-            immediately_calculate_pressure_matrix_numerically=False
+            immediately_calculate_pressure_matrix_numerically=False,
         )
 
 
@@ -131,9 +131,7 @@ def test_stiffness_matrix():
     Taken from example 5.5.1, page 181 (Dynamics of rotating machine, FRISSWELL)
     """
     bearing = fluid_flow_short_friswell()
-    kxx, kxy, kyx, kyy = calculate_stiffness_matrix(
-        bearing, force_type='short'
-    )
+    kxx, kxy, kyx, kyy = calculate_stiffness_matrix(bearing, force_type="short")
     assert math.isclose(kxx / 10 ** 6, 12.81, rel_tol=0.01)
     assert math.isclose(kxy / 10 ** 6, 16.39, rel_tol=0.01)
     assert math.isclose(kyx / 10 ** 6, -25.06, rel_tol=0.01)
@@ -149,10 +147,8 @@ def test_stiffness_matrix_numerical(fluid_flow_short_eccentricity):
     """
     bearing = fluid_flow_short_eccentricity
     bearing.calculate_pressure_matrix_numerical()
-    kxx, kxy, kyx, kyy = calculate_stiffness_matrix(bearing, force_type='numerical')
-    k_xx, k_xy, k_yx, k_yy = calculate_stiffness_matrix(
-        bearing, force_type='short'
-    )
+    kxx, kxy, kyx, kyy = calculate_stiffness_matrix(bearing, force_type="numerical")
+    k_xx, k_xy, k_yx, k_yy = calculate_stiffness_matrix(bearing, force_type="short")
     assert_allclose(kxx, k_xx, rtol=0.33)
     assert_allclose(kxy, k_xy, rtol=0.21)
     assert_allclose(kyx, k_yx, rtol=0.17)
@@ -166,9 +162,7 @@ def test_damping_matrix():
     Taken from example 5.5.1, page 181 (Dynamics of rotating machine, FRISSWELL)
     """
     bearing = fluid_flow_short_friswell()
-    cxx, cxy, cyx, cyy = calculate_damping_matrix(
-        bearing, force_type='short'
-    )
+    cxx, cxy, cyx, cyy = calculate_damping_matrix(bearing, force_type="short")
     assert math.isclose(cxx / 10 ** 3, 232.9, rel_tol=0.01)
     assert math.isclose(cxy / 10 ** 3, -81.92, rel_tol=0.01)
     assert math.isclose(cyx / 10 ** 3, -81.92, rel_tol=0.01)
@@ -203,7 +197,7 @@ def fluid_flow_short_numerical():
         rho,
         attitude_angle=attitude_angle,
         eccentricity=eccentricity,
-        immediately_calculate_pressure_matrix_numerically=False
+        immediately_calculate_pressure_matrix_numerically=False,
     )
 
 
@@ -234,7 +228,7 @@ def fluid_flow_long_numerical():
         visc,
         rho,
         eccentricity=eccentricity,
-        immediately_calculate_pressure_matrix_numerically=False
+        immediately_calculate_pressure_matrix_numerically=False,
     )
 
 
@@ -277,9 +271,12 @@ def test_oil_film_force_short():
     bearing = fluid_flow_short_numerical()
     bearing.calculate_pressure_matrix_numerical()
     n, t, force_x, force_y = calculate_oil_film_force(bearing)
-    n_numerical, t_numerical, force_x_numerical, force_y_numerical = calculate_oil_film_force(
-        bearing, force_type="numerical"
-    )
+    (
+        n_numerical,
+        t_numerical,
+        force_x_numerical,
+        force_y_numerical,
+    ) = calculate_oil_film_force(bearing, force_type="numerical")
     assert_allclose(n, n_numerical, rtol=0.5)
     assert_allclose(t, t_numerical, rtol=0.25)
     assert_allclose(force_x_numerical, 0, atol=1e-07)
@@ -290,9 +287,12 @@ def test_oil_film_force_long():
     bearing = fluid_flow_long_numerical()
     bearing.calculate_pressure_matrix_numerical()
     n, t, force_x, force_y = calculate_oil_film_force(bearing)
-    n_numerical, t_numerical, force_x_numerical, force_y_numerical = calculate_oil_film_force(
-        bearing, force_type="numerical"
-    )
+    (
+        n_numerical,
+        t_numerical,
+        force_x_numerical,
+        force_y_numerical,
+    ) = calculate_oil_film_force(bearing, force_type="numerical")
     assert_allclose(n, n_numerical, rtol=0.07)
     assert_allclose(t, t_numerical, rtol=0.22)
 
@@ -311,17 +311,31 @@ def test_matplotlib_plots():
     bearing = fluid_flow_short_numerical()
     bearing.calculate_pressure_matrix_analytical()
     ax_type = type(plt.gca())
-    assert isinstance(matplot_eccentricity(bearing), ax_type)
     assert isinstance(matplot_pressure_theta(bearing), ax_type)
     assert isinstance(matplot_pressure_z(bearing), ax_type)
     assert isinstance(matplot_shape(bearing), ax_type)
 
+    # Create new axes to avoid using gca(), which can get a axes with polar projection
+    fig, ax = plt.subplots()
+    ax_type = type(ax)
+    assert isinstance(matplot_eccentricity(bearing, ax=ax), ax_type)
+
 
 def test_find_equilibrium_position():
     bearing = flow.fluid_flow_example2()
-    find_equilibrium_position(bearing, print_along=False, tolerance=0.1, increment_factor=0.01,
-                              max_iterations=2, increment_reduction_limit=1e-03)
-    assert_allclose(bearing.eccentricity, (bearing.radius_stator - bearing.radius_rotor)*0.2663, atol=0.001)
+    find_equilibrium_position(
+        bearing,
+        print_along=False,
+        tolerance=0.1,
+        increment_factor=0.01,
+        max_iterations=2,
+        increment_reduction_limit=1e-03,
+    )
+    assert_allclose(
+        bearing.eccentricity,
+        (bearing.radius_stator - bearing.radius_rotor) * 0.2663,
+        atol=0.001,
+    )
 
 
 def test_move_rotor_center():
@@ -339,4 +353,3 @@ def test_move_rotor_center():
     move_rotor_center(bearing, 0, 0.001)
     assert bearing.eccentricity != eccentricity
     assert bearing.attitude_angle != attitude_angle
-
