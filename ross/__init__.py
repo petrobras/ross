4700c0545a82c491fa5ea5d8ab1c01f8a6f7a6fe
raphaeltimbo <raphaelts@gmail.com>
2020-06-09 19:50:43 -0300

    Set ross plotly theme as default

--- ross/__init__.py
+++ ross/__init__.py
@@ -1,5 +1,9 @@
 __version__ = "0.3.3"
 
+import plotly.io as _pio
+
+import ross.plotly_theme
+
 from .api_report import *
 from .bearing_seal_element import *
 from .disk_element import *
@@ -8,3 +12,5 @@ from .point_mass import *
 from .rotor_assembly import *
 from .shaft_element import *
 from .utils import visualize_matrix
+
+_pio.templates.default = "ross"
