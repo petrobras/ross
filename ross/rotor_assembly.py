12c5d9dfd2c10e485acb31d9787d827b019bcfa1
Rodrigo Martins <45969994+rodrigomoliveira1@users.noreply.github.com>
2020-06-10 16:26:31 -0300

    Improvements to run_unbalance_response()
    * add "modes" argument to run_unbalance_response, so the user can choose which modes will be used.
    * add unbalance argument to forced_response. It takes de nodes, magnitude and phase inputs to plot in ForcedResponseResults

--- ross/rotor_assembly.py
+++ ross/rotor_assembly.py
@@ -1160,7 +1160,7 @@ class Rotor(object):
 
         return results
 
-    def forced_response(self, force=None, speed_range=None, modes=None):
+    def forced_response(self, force=None, speed_range=None, modes=None, unbalance=None):
         """Unbalanced response for a mdof system.
 
         This method returns the unbalanced response for a mdof system
@@ -1176,6 +1176,11 @@ class Rotor(object):
         modes : list, optional
             Modes that will be used to calculate the frequency response
             (all modes will be used if a list is not given).
+        unbalance : array, optional
+            Array with the unbalance data (node, magnitude and phase) to be plotted
+            with deflected shape. This argument is set only if running an unbalance
+            response analysis.
+            Default is None.
 
         Returns
         -------
@@ -1207,13 +1212,12 @@ class Rotor(object):
             forced_resp[:, i] = freq_resp.freq_resp[..., i] @ force[..., i]
 
         forced_resp = ForcedResponseResults(
+            rotor=self,
             forced_resp=forced_resp,
             speed_range=speed_range,
             magnitude=abs(forced_resp),
             phase=np.angle(forced_resp),
-            nodes_list=self.nodes,
-            nodes_pos=self.nodes_pos,
-            number_dof=self.number_dof,
+            unbalance=unbalance,
         )
 
         return forced_resp
@@ -1265,7 +1269,9 @@ class Rotor(object):
 
         return F0
 
-    def run_unbalance_response(self, node, magnitude, phase, frequency_range=None):
+    def run_unbalance_response(
+        self, node, magnitude, phase, frequency_range=None, modes=None
+    ):
         """Unbalanced response for a mdof system.
 
         This method returns the unbalanced response for a mdof system
@@ -1282,6 +1288,9 @@ class Rotor(object):
             Unbalance phase (rad)
         frequency_range : list, float
             Array with the desired range of frequencies
+        modes : list, optional
+            Modes that will be used to calculate the frequency response
+            (all modes will be used if a list is not given).
 
         Returns
         -------
@@ -1309,6 +1318,10 @@ class Rotor(object):
 
         # plot unbalance response:
         >>> fig = response.plot(dof=13)
+        
+        # plot deflected shape configuration
+        >>> speed = 600
+        >>> fig = response.response.plot_deflected_shape(speed=speed)
         """
         force = np.zeros((self.ndof, len(frequency_range)), dtype=np.complex)
 
@@ -1318,7 +1331,8 @@ class Rotor(object):
         except TypeError:
             force = self._unbalance_force(node, magnitude, phase, frequency_range)
 
-        forced_response = self.forced_response(force, frequency_range)
+        ub = np.vstack((node, magnitude, phase))
+        forced_response = self.forced_response(force, frequency_range, modes, ub)
 
         return forced_response
 
