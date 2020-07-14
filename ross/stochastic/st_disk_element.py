97f66366d2e4bfe69881af983b037866357866ff
Rodrigo Martins <45969994+rodrigomoliveira1@users.noreply.github.com>
2020-07-14 09:29:27 -0300

    Minor changes on plot_random_var
    * Change default option for plotting all the random variables.

--- ross/stochastic/st_disk_element.py
+++ ross/stochastic/st_disk_element.py
@@ -185,7 +185,7 @@ class ST_DiskElement:
 
         return f_list
 
-    def plot_random_var(self, var_list=[], histogram_kwargs={}, plot_kwargs={}):
+    def plot_random_var(self, var_list=None, histogram_kwargs=None, plot_kwargs=None):
         """Plot histogram and the PDF.
 
         This function creates a histogram to display the random variable
@@ -195,6 +195,7 @@ class ST_DiskElement:
         ----------
         var_list : list, optional
             List of random variables, in string format, to plot.
+            Default is plotting all the random variables.
         histogram_kwargs : dict, optional
             Additional key word arguments can be passed to change
             the plotly.go.histogram (e.g. histnorm="probability density", nbinsx=20...).
@@ -219,9 +220,11 @@ class ST_DiskElement:
         label = dict(
             m="Mass", Id="Diametral moment of inertia", Ip="Polar moment of inertia",
         )
-        if not all(var in self.is_random for var in var_list):
+        if var_list is None:
+            var_list = self.is_random
+        elif not all(var in self.is_random for var in var_list):
             raise ValueError(
-                "Not random variable in var_list. Select variables from {}".format(
+                "Random variable not in var_list. Select variables from {}".format(
                     self.is_random
                 )
             )
