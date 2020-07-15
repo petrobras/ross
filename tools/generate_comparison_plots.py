6642ff22810edc97d5e8758af937d64e354d9bc5
raphaeltimbo <raphaelts@gmail.com>
2020-07-15 18:27:13 -0300

    Add script to generate plots for comparison
    This adds a script that generates some plots for 3 different rotor
    models. This helps to solve a problem that we had when we modified some
    property to improve the plot for a specific model and later we would
    notice that at the end the modification would be detrimental to other
    models.
    This, together with the plotly_theme style, closes #580.
    Now we should try to make all plot modifications on the plotly_theme.py
    file, and we should run the generate_comparison_plots.py script and
    check the output.
    After running the script we can open the index.html file and click each
    link to check the specific plot for the three models.
    This script has been added to a tools directory inside the repository.
    The tools directory will be used to store some utility used for the
    development of our code. This is the same structure that other projects
    such as numpy use.
    I have added the generated html files to the .gitignore files, since
    they should always be generated by the script (with the exception of
    the index.html file).

---
+++ tools/generate_comparison_plots.py
@@ -0,0 +1,118 @@
+from pathlib import Path
+
+import numpy as np
+import plotly.io as pio
+from jinja2 import Environment, FileSystemLoader
+
+import ross as rs
+
+dir_path = Path(__file__).parent
+rotors = {
+    k: rs.Rotor.load((dir_path / "data" / k))
+    for k in ["rotor_example", "c123701", "injection"]
+}
+
+env = Environment(loader=FileSystemLoader(dir_path / "plots/templates/"))
+template = env.get_template("template.html")
+
+
+# plot_rotor
+kwargs = {
+    rotor: pio.to_html(
+        rotors[rotor].plot_rotor(), include_plotlyjs=False, full_html=False
+    )
+    for rotor in rotors
+}
+output = template.render(**kwargs)
+
+with open(f"{dir_path}/plots/plot_rotor.html", "w") as f:
+    f.write(output)
+
+# plot_ucs
+kwargs = {
+    rotor: pio.to_html(
+        rotors[rotor].plot_ucs(), include_plotlyjs=False, full_html=False
+    )
+    for rotor in rotors
+}
+output = template.render(**kwargs)
+
+with open(f"{dir_path}/plots/plot_ucs.html", "w") as f:
+    f.write(output)
+
+    # plot_mode_2d
+    kwargs = {
+        rotor: pio.to_html(
+            rotors[rotor].run_modal(speed=100).plot_mode_2d(0),
+            include_plotlyjs=False,
+            full_html=False,
+        )
+        for rotor in rotors
+    }
+    output = template.render(**kwargs)
+
+    with open(f"{dir_path}/plots/plot_mode_2d.html", "w") as f:
+        f.write(output)
+
+# plot_mode_3d
+kwargs = {
+    rotor: pio.to_html(
+        rotors[rotor].run_modal(speed=100).plot_mode_3d(0),
+        include_plotlyjs=False,
+        full_html=False,
+    )
+    for rotor in rotors
+}
+output = template.render(**kwargs)
+
+with open(f"{dir_path}/plots/plot_mode_3d.html", "w") as f:
+    f.write(output)
+
+# unbalance.plot
+kwargs = {
+    rotor: pio.to_html(
+        rotors[rotor]
+        .run_unbalance_response(
+            node=0,
+            unbalance_magnitude=0.5,
+            unbalance_phase=0,
+            frequency=np.linspace(0, 2000),
+        )
+        .plot(0),
+        include_plotlyjs=False,
+        full_html=False,
+    )
+    for rotor in rotors
+}
+output = template.render(**kwargs)
+
+with open(f"{dir_path}/plots/plot_unbalance.html", "w") as f:
+    f.write(output)
+
+# campbell.plot
+kwargs = {
+    rotor: pio.to_html(
+        rotors[rotor].run_campbell(speed_range=np.linspace(0, 2000)).plot(),
+        include_plotlyjs=False,
+        full_html=False,
+    )
+    for rotor in rotors
+}
+output = template.render(**kwargs)
+
+with open(f"{dir_path}/plots/plot_campbell.html", "w") as f:
+    f.write(output)
+
+# static.plot_deformation
+kwargs = {
+    rotor: pio.to_html(
+        rotors[rotor].run_static().plot_deformation(),
+        include_plotlyjs=False,
+        full_html=False,
+    )
+    for rotor in rotors
+}
+output = template.render(**kwargs)
+
+with open(f"{dir_path}/plots/plot_deformation.html", "w") as f:
+    f.write(output)
