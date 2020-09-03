9dfbce997c8245067b6a061eac158fab9f407881
Marcus Filipe <marcusfilipesr@gmail.com>
2020-09-03 01:12:23 -0300

    Misalingment Flex: time integration implemented

--- ross/rotor_assembly.py
+++ ross/rotor_assembly.py
@@ -2364,14 +2364,7 @@ class Rotor(object):
 
         return results
 
-    def run_misalignment_flex(self, misalignment):
-        radius = self.df_shaft.iloc[misalignment.n1].o_d / 2
-        F = misalignment.run(radius, self.ndof)
-        return self.run_time_response(
-            misalignment.speed * np.pi / 30, F, misalignment.t
-        )
-
-    def run_misalignment_rigid(self, misalignment):
+    def run_misalignment(self, misalignment):
         misalignment.run(self)
         return misalignment
 
