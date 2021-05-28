59a3cf0a371d2ebc4c776271bb47e3ea17ee4c8e
Vergilio Del Claro <vergiliotorezan@gmail.com>
2021-05-28 09:02:11 -0300

    Rough translation finished

--- ross/fluid_flow/thrust_bearing.py
+++ ross/fluid_flow/thrust_bearing.py
@@ -1596,39 +1596,70 @@ def thrust_bearing_example():
     0.263144
     """
 
+    r1 = (0.5 * 90e-3,)  # pad inner radius [m]
+    r2 = (0.5 * 160e-3,)  # pad outer radius [m]
+    rp = ((r2 - r1) * 0.5 + r1,)  # pad pivot radius [m]
+    teta0 = (35 * np.pi / 180,)  # pad complete angle [rad]
+    tetap = (19.5 * np.pi / 180,)  # pad pivot angle [rad]
+    TC = (40 + 273.15,)  # Collar temperature [K]
+    Tin = (40 + 273.15,)  # Cold oil temperature [K]
+    T0 = (0.5 * (TC + Tin),)  # Reference temperature [K]
+    rho = (870,)  # Oil density [kg/m³]
+    cp = (1850,)  # Oil thermal capacity [J/kg/K]
+    kt = (0.15,)  # Oil thermal conductivity [W/m/K]
+    k1 = (0.06246,)  # Coefficient for ISO VG 32 turbine oil - Vogel's equation
+    k2 = (868.8,)  # Coefficient for ISO VG 32 turbine oil - Vogel's equation
+    k3 = (170.4,)  # Coefficient for ISO VG 32 turbine oil - Vogel's equation
+    mi0 = (1e-6 * rho * 22,)  # Oil VG 22
+    fz = (370 * 9.81,)  # Loading in Y direction [N]
+    Npad = (3,)  # Number of PADs
+    NTETA = (40,)  # TETA direction N volumes
+    NR = (40,)  # R direction N volumes
+    war = ((1200 * np.pi) / 30,)  # Shaft rotation speed [RPM]
+    R1 = (1,)  # Inner pad FEM radius
+    R2 = (r2 / r1,)  # Outer pad FEM radius
+    TETA1 = (0,)  # Initial angular coordinate
+    TETA2 = (1,)  # Final angular coordinate
+    Rp = (rp / r1,)  # Radial pivot position
+    TETAp = (tetap / teta0,)  # Angular pivot position
+    dR = ((R2 - R1) / (NR),)  # R direction volumes length
+    dTETA = ((TETA2 - TETA1) / (NTETA),)  # TETA direction volumes length
+    Ti = (T0 * (1 + np.zeros(NR, NTETA)),)  # Initial temperature field [°C]
+    x0 = (
+        np.array(-2.251004554793839e-04, -1.332796067467349e-04, 2.152552477569639e-05),
+    )  # Initial equilibrium position
+
     bearing = Thrust(
-        r1=0.5 * 90e-3,  # pad inner radius [m]
-        r2=0.5 * 160e-3,  # pad outer radius [m]
-        rp=(r2 - r1) * 0.5 + r1,  # pad pivot radius [m]
-        teta0=35 * pi / 180,  # pad complete angle [rad]
-        tetap=19.5 * pi / 180,  # pad pivot angle [rad]
-        TC=40 + 273.15,  # Collar temperature [K]
-        Tin=40 + 273.15,  # Cold oil temperature [K]
-        T0=0.5 * (TC + Tin),  # Reference temperature [K]
-        rho=870,  # Oil density [kg/m³]
-        cp=1850,  # Oil thermal capacity [J/kg/K]
-        kt=0.15,  # Oil thermal conductivity [W/m/K]
-        k1=0.06246,  # Coefficient for ISO VG 32 turbine oil - Vogel's equation
-        k2=868.8,  # Coefficient for ISO VG 32 turbine oil - Vogel's equation
-        k3=170.4,  # Coefficient for ISO VG 32 turbine oil - Vogel's equation
-        mi0=1e-6 * rho * 22,  # Oil VG 22
-        fz=370 * 9.81,  # Loading in Y direction [N]
-        Npad=3,  # Number of PADs
-        NTETA=40,  # TETA direction N volumes
-        NR=40,  # R direction N volumes
-        war=(1200 * pi) / 30,  # Shaft rotation speed [RPM]
-        R1=1,  # Inner pad FEM radius
-        R2=r2 / r1,  # Outer pad FEM radius
-        TETA1=0,  # Initial angular coordinate
-        TETA2=1,  # Final angular coordinate
-        Rp=rp / r1,  # Radial pivot position
-        TETAp=tetap / teta0,  # Angular pivot position
-        dR=(R2 - R1) / (NR),  # R direction volumes length
-        dTETA=(TETA2 - TETA1) / (NTETA),  # TETA direction volumes length
-        Ti=T0 * ones(NR, NTETA),  # Initial temperature field [°C]
-        x0=np.array(
-            -2.251004554793839e-04, -1.332796067467349e-04, 2.152552477569639e-05
-        ),  # Initial equilibrium position
+        r1=r1,
+        r2=r2,
+        rp=rp,
+        teta0=teta0,
+        tetap=tetap,
+        TC=TC,
+        Tin=Tin,
+        T0=T0,
+        rho=rho,
+        cp=cp,
+        kt=kt,
+        k1=k1,
+        k2=k2,
+        k3=k3,
+        mi0=mi0,
+        fz=fz,
+        Npad=Npad,
+        NTETA=NTETA,
+        NR=NR,
+        war=war,
+        R1=R1,
+        R2=R2,
+        TETA1=TETA1,
+        TETA2=TETA2,
+        Rp=Rp,
+        TETAp=TETAp,
+        dR=dR,
+        dTETA=dTETA,
+        Ti=Ti,
+        x0=x0,
     )
 
     return bearing
