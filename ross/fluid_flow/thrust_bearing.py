ea66af7823c7ba9265cc24d70aaf7ae0bde4a6a8
Vergilio Del Claro <vergiliotorezan@gmail.com>
2021-06-21 11:11:17 -0300

    vec_R and vec_TETA declarations fix

--- ross/fluid_flow/thrust_bearing.py
+++ ross/fluid_flow/thrust_bearing.py
@@ -92,7 +92,9 @@ class Thrust:
         # --------------------------------------------------------------------------
         # Pre-processing loop counters for ease of understanding
         vec_R = np.arange((R1 + 0.5 * dR), (R2 - 0.5 * dR), dR)
+        vec_R = np.append(vec_R, [R2 - 0.5 * dR])
         vec_TETA = np.arange((TETA1 + 0.5 * dTETA), (TETA2 - 0.5 * dTETA), dTETA)
+        vec_TETA = np.append(vec_TETA, [TETA2 - 0.5 * dTETA])
 
         # --------------------------------------------------------------------------
         # WHILE LOOP INITIALIZATION
