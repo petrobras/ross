dbf922a5a0726e45514dc327fc36e4b15920062b
Vergilio Del Claro <vergiliotorezan@gmail.com>
2021-07-09 11:07:43 -0300

    Comments correction on test function

--- ross/rotor_assembly.py
+++ ross/rotor_assembly.py
@@ -3829,8 +3829,19 @@ def rotor_example_6dof():
     >>> rotor6 = rs.rotor_assembly.rotor_example_6dof()
     >>> camp6 = rotor6.run_campbell(np.linspace(0,400,101),frequencies=18)
 
-    # plotting Campbell Diagram
+    Plotting rotor model
+    >>> fig = rotor6.plot_rotor()
+    >>> fig.show()
+
+    Running modal
+    >>> rotor_speed = 100.0 # rad/s
+    >>> modal6 = rotor6.run_modal(rotor_speed)
+    >>> print(f"Undamped natural frequencies:\n {modal6.wn}")
+    >>> print(f"Damped natural frequencies:\n {modal6.wd}")
+
+    Plotting Campbell Diagram
     >>> fig = camp6.plot()
+    >>> fig.show()
     """
     #  Rotor with 6 DoFs, with internal damping, with 10 shaft elements, 2 disks and 2 bearings.
     i_d = 0
