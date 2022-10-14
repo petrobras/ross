70c99a84cc467d5b3dce5ffdd17bc0e118248b9e
Marcus Filipe <marcusfilipesr@gmail.com>
2022-10-14 16:14:12 -0300

    fixed the materials test

--- ross/tests/test_materials.py
+++ ross/tests/test_materials.py
@@ -57,15 +57,7 @@ def test_specific_material(AISI4140):
 def test_error_rho():
     with pytest.raises(TypeError) as ex:
         Material(name="test", E=203.2e9, G_s=80e9)
-    if sys.version_info.minor == 10:
-        assert (
-            "Material.__init__() missing 1 required positional argument: 'rho'"
-            == str(ex.value)
-        )
-    else:
-        assert "__init__() missing 1 required positional argument: 'rho'" == str(
-            ex.value
-        )
+    assert "__init__() missing 1 required positional argument: 'rho'" in str(ex.value)
 
 
 def test_error_E_G_s_Poisson():
