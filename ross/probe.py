79d105ce811bf9941aea5bcdaca45904a0e62b4d
raphaeltimbo <raphaelts@petrobras.com.br>
2023-04-27 16:35:10 -0300

    Changes to docstring

--- ross/probe.py
+++ ross/probe.py
@@ -4,20 +4,27 @@ from ross.units import Q_, check_units
 class Probe:
     """Class of a probe.
 
+    This class will create a probe object to be used in the rotor model.
+    The probe is used to measure the response of the rotor at a specific
+    location and orientation.
+
     Parameters
     ----------
     node : int
         Indicate the node where the probe is located.
     angle : float, pint.Quantity
-        Probe orientation angle about the shaft.
+        Probe orientation angle about the shaft (rad).
     tag : str, optional
-        Probe tag to be add a DataFrame column title.
+        A tag to name the element.
 
     Example
     -------
-    >>> probe1 = Probe(10,Q_(45,"degree"),"V1")
-    >>> probe1.info
-    (10, 0.7853981633974483)
+    >>> import ross as rs
+    >>> probe1 = rs.Probe(10, Q_(45, "degree"), "Probe Drive End - X")
+    >>> probe1.node
+    10
+    >>> probe1.angle
+    0.7853981633974483
     """
 
     @check_units
@@ -29,7 +36,7 @@ class Probe:
 
     @property
     def info(self):
-        return (self.node, self.angle)
+        return self.node, self.angle
 
     def __str__(self):
         return (
