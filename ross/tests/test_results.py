28432f2b6a10623c6ca27a472791f810747cb570
ViniciusTxC3 <viniciustxc@yahoo.com.br>
2024-03-22 15:46:57 -0300

    black

--- ross/tests/test_results.py
+++ ross/tests/test_results.py
@@ -205,22 +205,10 @@ def test_probe_response(rotor1):
     probe1 = (3, 0)  # node 3, orientation 0° (X dir.)
     probe2 = (3, 90)  # node 3, orientation 90°(Y dir.)
     resp_prob1 = np.array(
-        [
-            0.00000000e00,
-            4.07504756e-06,
-            1.19778973e-05,
-            1.68562228e-05,
-            1.34097882e-05
-        ]
+        [0.00000000e00, 4.07504756e-06, 1.19778973e-05, 1.68562228e-05, 1.34097882e-05]
     )
     resp_prob2 = np.array(
-        [
-            0.00000000e00,
-            4.13295078e-06,
-            8.25529257e-06,
-            1.28932310e-05,
-            1.59791798e-05
-        ]
+        [0.00000000e00, 4.13295078e-06, 8.25529257e-06, 1.28932310e-05, 1.59791798e-05]
     )
     data = response.data_probe_response(probe=[probe1, probe2], probe_units="degree")
     assert_allclose(data["probe_resp[0]"].to_numpy()[:5], resp_prob1)
