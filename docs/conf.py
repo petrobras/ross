0170c513a372b11525a5a27ff4e5fde242b53b68
raphaeltimbo <raphaelts@petrobras.com.br>
2025-04-07 11:35:16 -0300

    Change language = None to 'en'
    The value language = None is deprecated and falls back to english as a
    default.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ import ross
 
 # General information about the project.
 project = "ross"
-copyright = "2024"
+copyright = "2025"
 author = "ROSS devs"
 googleanalytics_id = "G-YKZBE83MZ5"
 
@@ -82,7 +82,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
