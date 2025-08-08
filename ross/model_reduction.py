d7878958bab3f2027ebbfc27b9ef63fe9e56714f
jguarato <jguaratofs@gmail.com>
2025-08-08 13:41:35 -0300

    Create function to select dofs based on disks and bearings

--- ross/model_reduction.py
+++ ross/model_reduction.py
@@ -1,4 +1,7 @@
 import numpy as np
+from ross import Rotor
+
+from copy import deepcopy
 
 
 class ModelReduction:
@@ -6,7 +9,7 @@ class ModelReduction:
     Base class for model reduction methods.
     """
 
-    def __init__(self, K, M, method="guyan"):
+    def __init__(self, rotor, important_nodes=[], method="guyan"):
         """
         Initialize the model reduction with a given model.
 
@@ -16,12 +19,14 @@ class ModelReduction:
             The rotor model to be reduced.
         """
 
-        self.M = M
-        self.K = K
+        self.ndof = rotor.ndof
+        self.number_dof = rotor.number_dof
+        self.K = rotor.K(0)
+        self.M = rotor.M()
 
-        self.ndof = K.shape[0]
+        self.orig_rotor = rotor
 
-        M_K = np.diag(K) / np.diag(M)
+        M_K = np.diag(self.K) / np.diag(self.M)
         # Sort DOFs by mass-stiffness ratio (M/K)
         self.indx = np.argsort(M_K)[::-1]
 
@@ -32,11 +37,111 @@ class ModelReduction:
         else:
             raise ValueError(f"Pass a existing {method}.")
 
-        self.run()
+        self.slave_dofs, self.retained_dofs = self.select_nodes_based_rotor(
+            rotor, important_nodes
+        )
+        self.reduced_rotor = self.reduce_model(self.slave_dofs, self.retained_dofs)
 
-        return
+    @staticmethod
+    def copy_rotor(rotor, tag=None):
+        return Rotor(
+            deepcopy(rotor.shaft_elements),
+            disk_elements=deepcopy(rotor.disk_elements),
+            bearing_elements=deepcopy(rotor.bearing_elements),
+            point_mass_elements=deepcopy(rotor.point_mass_elements),
+            min_w=rotor.min_w,
+            max_w=rotor.max_w,
+            rated_w=rotor.rated_w,
+            tag=tag or rotor.tag,
+        )
 
-    def run(self):
+    @staticmethod
+    def select_nodes_based_rotor(rotor, important_nodes=[]):
+        ignored_dofs = []
+        selected_dofs = []
+
+        important_nodes.append(min(rotor.nodes))
+        important_nodes.append(max(rotor.nodes))
+
+        for elm in rotor.elements:
+            if elm.n not in rotor.nodes:
+                continue
+
+            dofs = list(elm.dof_global_index.values())
+
+            if elm in rotor.shaft_elements and elm.n not in important_nodes:
+                ignored_dofs.extend(dofs)
+            else:
+                selected_dofs.extend(dofs)
+
+        return sorted(set(ignored_dofs)), sorted(set(selected_dofs))
+
+    @staticmethod
+    def rearrange_matrix(matrix, slave_dofs, retained_dofs):
+        return np.block(
+            [
+                [
+                    matrix[np.ix_(retained_dofs, retained_dofs)],
+                    matrix[np.ix_(retained_dofs, slave_dofs)],
+                ],
+                [
+                    matrix[np.ix_(slave_dofs, retained_dofs)],
+                    matrix[np.ix_(slave_dofs, slave_dofs)],
+                ],
+            ]
+        )
+
+    def get_transformation_functions(self, transf_matrix, slave_dofs, retained_dofs):
+        # Verificar - talvez seja melhor separar em várias funções
+        reduce_matrix = (
+            lambda array: (
+                transf_matrix.T
+                @ self.rearrange_matrix(array, slave_dofs, retained_dofs)
+            )
+            @ transf_matrix
+        )
+
+        reduce_vector = lambda array: transf_matrix.T @ self.rearrange_matrix(
+            array, slave_dofs, retained_dofs
+        )
+
+        get_complete_vector = lambda array: transf_matrix @ array
+
+        return reduce_matrix, reduce_vector, get_complete_vector
+
+    def increment_nodes(self, add_nodes=[]):
+        num_dof = self.number_dof
+
+        for n in add_nodes:
+            dofs = range(n * num_dof + 0, n * num_dof + num_dof)
+            self.slaves_dofs.extends(dofs)
+
+            for dof in dofs:
+                self.retained_dofs.remove(dof)
+
+        self.reduce_model(self.slave_dofs, self.retained_dofs)
+
+    def reduce_model(self, slave_dofs, retained_dofs):
+        transf_matrix = self.model_reduction_technique(slave_dofs, retained_dofs)
+        functions = self.get_transformation_functions(
+            transf_matrix, slave_dofs, retained_dofs
+        )
+        reduce_matrix = functions[0]
+
+        rotor = self.orig_rotor
+        reduced_rotor = self.copy_rotor(rotor)
+
+        reduced_rotor.M = lambda frequency=None, synchronous=False: reduce_matrix(
+            rotor.M(frequency=frequency, synchronous=synchronous)
+        )
+        reduced_rotor.K = lambda frequency: reduce_matrix(rotor.K(frequency))
+        reduced_rotor.Ksdt = lambda: reduce_matrix(rotor.Ksdt())
+        reduced_rotor.C = lambda frequency: reduce_matrix(rotor.C(frequency))
+        reduced_rotor.G = lambda: reduce_matrix(rotor.G())
+
+        return reduced_rotor
+
+    def optimize(self):
         n = 0
         aux_count = 0
         alldofs = np.arange(self.ndof)
@@ -76,41 +181,11 @@ class ModelReduction:
             n += 3
             aux_count += 1
 
-    def remove_dofs_from_matrix(matrix, slave_dofs, retained_dofs):
-        return np.block(
-            [
-                [
-                    matrix[np.ix_(retained_dofs, retained_dofs)],
-                    matrix[np.ix_(retained_dofs, slave_dofs)],
-                ],
-                [
-                    matrix[np.ix_(slave_dofs, retained_dofs)],
-                    matrix[np.ix_(slave_dofs, slave_dofs)],
-                ],
-            ]
-        )
-
-    def get_transformation_functions(self, transformation_matrix):
-        reduce_matrix = (
-            lambda array: (
-                transformation_matrix.T @ self.remove_dofs_from_matrix(array)
-            )
-            @ transformation_matrix
-        )
-        reduce_vector = (
-            lambda array: transformation_matrix.T @ self.remove_dofs_from_matrix(array)
-        )
-        get_complete_vector = lambda array: transformation_matrix @ array
-
-        return reduce_matrix, reduce_vector, get_complete_vector
-
-    def guyan(self):
+    def guyan(self, slave_dofs, retained_dofs):
         """
         Standard Guyan Reduction method.
         """
         K = self.K
-        slave_dofs = self.slave_dofs
-        retained_dofs = self.retained_dofs
 
         # Compute transformation matrix
         Kia = (
@@ -119,13 +194,11 @@ class ModelReduction:
         )
         Tg = np.block([[np.eye(Kia.shape[1])], [Kia]])
 
-        return self.get_transformation_functions(Tg)
+        return Tg
 
-    def guyan_melhorado(self, speed, slave_dofs, retained_dofs):
+    def guyan_melhorado(self, slave_dofs, retained_dofs):
         M = self.M
         K = self.K
-        slave_dofs = self.slave_dofs
-        retained_dofs = self.retained_dofs
 
         # Compute transformation matrix using inverse of slave stiffness matrix
         Kia = (
@@ -147,12 +220,12 @@ class ModelReduction:
             Kfi[start:, start:] = Kss
 
         # Reduced mass and stiffness matrices via Guyan transformation
-        Mrr = self.get_red_matrix(M, slave_dofs, retained_dofs)
-        Krr = self.get_red_matrix(K, slave_dofs, retained_dofs)
+        Mrr = self.rearrange_matrix(M, slave_dofs, retained_dofs)
+        Krr = self.rearrange_matrix(K, slave_dofs, retained_dofs)
         Mr = Tg.T @ Mrr @ Tg
         Kr = Tg.T @ Krr @ Tg
 
         # IRS transformation (Improved Reduced System)
         Tirs = Tg + Kfi @ M @ Tg @ np.linalg.inv(Mr) @ Kr
 
-        return self.get_transformation_functions(Tirs)
+        return Tirs
