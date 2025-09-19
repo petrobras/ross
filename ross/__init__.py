db2a08ed09ee4478bd4f9cd07ae003585b69f698
gsabino <gabriel.sabino.prestserv@petrobras.com.br>
2025-09-19 11:50:47 -0300

    fix: add ThrustPad import to __init__.py

--- ross/__init__.py
+++ ross/__init__.py
@@ -21,6 +21,7 @@ from .utils import get_data_from_figure, visualize_matrix
 from ross.bearings.lubricants import lubricants_dict
 from ross.bearings.cylindrical import *
 from ross.seals.labyrinth_seal import *
+from ross.bearings.thrust_pad import *
 
 _pio.templates.default = "ross"
 if "ipykernel" in sys.modules:
