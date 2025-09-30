ef6030732cbd808d088990bdf0d40819a1c71ecf
jguarato <jguaratofs@gmail.com>
2025-09-30 15:23:28 -0300

    Make _build_harmonic_balance_matrix static

--- ross/harmonic_balance.py
+++ ross/harmonic_balance.py
@@ -15,7 +15,7 @@ class HarmonicBalance:
     def __init__(self, rotor):
         self.rotor = rotor
         self.ndof = rotor.ndof
-        self.probeForce = None
+        self.probe_force = None
         self.noh = 1
 
     @check_units
@@ -28,21 +28,18 @@ class HarmonicBalance:
         t,
         n_harmonics=1,
         F_ext=None,
-        points=None,
-        crack=None,
     ):
         rotor = self.rotor
         dt = t[1] - t[0]
-        accel = 0
         ndof = rotor.ndof
         self.noh = n_harmonics
 
-        if points is None or points > len(t):
-            points = int(len(t) / 2)
-        self.points = points
+        accel = 0
+        has_gravity = 0
+        has_crack = None
 
         # Forces
-        W = rotor.gravitational_force() * 0
+        W = rotor.gravitational_force() * has_gravity
         F_unb, F_unb_s = self._unbalance_force(node, unb_magnitude, unb_phase, speed)
 
         if F_ext is None:
@@ -54,15 +51,26 @@ class HarmonicBalance:
         F = self._assemble_forces(W, F_unb, F_unb_s, Fo, Fn, Fn_s)
 
         # Crack stiffness matrices
-        if crack:
+        if has_crack:
             Ko, Kn, Kn_s = self._crack_stiffness_matrices()
         else:
             Ko = np.zeros((ndof, ndof), dtype=complex)
             Kn = np.zeros((ndof, ndof, 2 * self.noh), dtype=complex)
             Kn_s = np.zeros((ndof, ndof, 2 * self.noh), dtype=complex)
 
+        # Co, Cn, Cn_s are already considered in C matrix (bearing elements)
+        Co = np.zeros((ndof, ndof), dtype=complex)
+        Cn = np.zeros((ndof, ndof, 2 * self.noh), dtype=complex)
+        Cn_s = np.zeros((ndof, ndof, 2 * self.noh), dtype=complex)
+
+        # alpha and beta are already considered in C matrix (shaft elements)
+        alpha = 0
+        beta = 0
+
         # Harmonic Balance Matrix
         H = self._build_harmonic_balance_matrix(
+            ndof,
+            self.noh,
             speed,
             accel,
             rotor.M(speed),
@@ -73,6 +81,11 @@ class HarmonicBalance:
             Ko,
             Kn,
             Kn_s,
+            Co,
+            Cn,
+            Cn_s,
+            alpha,
+            beta,
         )
 
         Qt, Qo, dQ, dQ_s = self._solve_freq_response(H, F)
@@ -87,7 +100,6 @@ class HarmonicBalance:
         speed_range,
         n_harmonics=1,
         F_ext=None,
-        points=None,
     ):
         ndof = self.rotor.ndof
         t = np.linspace(0, 2 * np.pi / speed_range[-1], 1000)  # ??
@@ -105,7 +117,6 @@ class HarmonicBalance:
                 t,
                 n_harmonics=n_harmonics,
                 F_ext=F_ext,
-                points=points,
             )
 
         unbalance = np.vstack((node, unb_magnitude, unb_phase))
@@ -130,7 +141,6 @@ class HarmonicBalance:
         t,
         n_harmonics=1,
         F_ext=None,
-        points=None,
     ):
         _, Qo, dQ, _ = self.run(
             node,
@@ -140,7 +150,6 @@ class HarmonicBalance:
             t,
             n_harmonics=n_harmonics,
             F_ext=F_ext,
-            points=points,
         )
 
         y, ydot, y2dot = self._reconstruct_time_domain(speed, t, Qo, dQ)
@@ -195,28 +204,41 @@ class HarmonicBalance:
     def _external_force(self, F_ext, dt, freq):
         ndof = self.rotor.ndof
         number_dof = self.rotor.number_dof
-
-        t_size = F_ext.shape[1]
+        points = F_ext.shape[0] // 2
 
         Fo = np.zeros(ndof, dtype=complex)
         F = np.zeros((ndof, self.noh), dtype=complex)
         F_s = np.zeros((ndof, self.noh), dtype=complex)
 
-        if self.probeForce:
-            for i in range(len(self.probeForce)):
+        if self.probe_force:
+            for i in range(len(self.probe_force)):
                 idx = slice(
-                    (self.probeForce[i]) * number_dof - 6,
-                    (self.probeForce[i]) * number_dof,
+                    (self.probe_force[i]) * number_dof - 6,
+                    (self.probe_force[i]) * number_dof,
                 )
 
-                Fnew = F_ext[idx, -(t_size - self.points) :]
-                Fo, Fn = self._expand_Fourier(Fnew, dt, freq)
+                Fnew = F_ext[idx, points:]
+                Fo, Fn = self._expand_Fourier(Fnew, dt, freq, self.noh)
 
                 Fo[idx] += Fo
                 F[idx, :] += Fn
                 F_s[idx, :] += np.conjugate(Fn)
 
         return Fo, F, F_s
+
+    def _assemble_forces(self, W, F_unb, F_unb_s, Fo, Fn, Fn_s):
+        ndof = self.rotor.ndof
+        F0 = np.zeros(((self.noh * 2 + 1) * ndof), dtype=complex)
+
+        F0[:ndof] = 4 * W + 2 * Fo
+        F0[ndof : 2 * ndof] = 2 * F_unb + 2 * Fn[:, 0]
+        F0[2 * ndof : 3 * ndof] = 2 * F_unb_s + 2 * Fn_s[:, 0]
+
+        for i in range(2, self.noh + 1):
+            F0[(2 * i - 1) * ndof : 2 * i * ndof] = 2 * Fn[:, i - 1]
+            F0[2 * i * ndof : (2 * i + 1) * ndof] = 2 * Fn_s[:, i - 1]
+
+        return F0
 
     def _crack_stiffness_matrices(self, dt, freq):
         ndof = self.rotor.ndof
@@ -240,182 +262,6 @@ class HarmonicBalance:
             Kn_s[idx, idx, i] = Kn_flex_s[:, :, i]
 
         return Ko, Kn, Kn_s
-
-    @check_units
-    def _expand_Fourier(self, F, dt, fo, noh=None):
-        if noh is None:
-            noh = self.noh
-
-        row, N = F.shape
-        Fn = np.array(np.zeros((row, noh)), dtype=complex)
-
-        b = int(np.floor(N / 2))
-        df = 1 / (N * dt)
-
-        A = fft(F)
-        X = A[:, :b]
-        X = X * 2 / N
-        Fo = 1 * np.real(X[:, 0])
-        an = 1 * np.real(X)
-        bn = -1 * np.imag(X)
-
-        for n in range(1, noh + 1):
-            idx = int(n * fo / df)
-            Fn[:, n - 1] = an[:, idx] - 1j * bn[:, idx]
-
-        return Fo, Fn
-
-    def _assemble_forces(self, W, F_unb, F_unb_s, Fo, Fn, Fn_s):
-        ndof = self.rotor.ndof
-
-        F0 = np.zeros(((self.noh * 2 + 1) * ndof), dtype=complex)
-
-        F0[:ndof] = 4 * W + 2 * Fo
-        F0[ndof : 2 * ndof] = 2 * F_unb + 2 * Fn[:, 0]
-        F0[2 * ndof : 3 * ndof] = 2 * F_unb_s + 2 * Fn_s[:, 0]
-
-        for i in range(2, self.noh + 1):
-            F0[(2 * i - 1) * ndof : 2 * i * ndof] = 2 * Fn[:, i - 1]
-            F0[2 * i * ndof : (2 * i + 1) * ndof] = 2 * Fn_s[:, i - 1]
-
-        return F0
-
-    def _build_harmonic_balance_matrix(
-        self,
-        speed,
-        accel,
-        M,
-        K,
-        Ksdt,
-        C,
-        G,
-        Ko,
-        Kn,
-        Kn_s,
-    ):
-        ndof = self.rotor.ndof
-
-        # alpha and beta are already considered in C matrix (shaft elements)
-        alpha = 0
-        beta = 0
-
-        # Co, Cn, Cn_s are already considered in C matrix (bearing elements)
-        Co = np.zeros((ndof, ndof), dtype=complex)
-        Cn = np.zeros((ndof, ndof, 2 * self.noh), dtype=complex)
-        Cn_s = np.zeros((ndof, ndof, 2 * self.noh), dtype=complex)
-
-        size = ndof * (2 * self.noh + 1)
-        H0 = np.zeros((size, size), dtype=complex)
-
-        idx0 = slice(0, ndof)
-        idx1 = slice(1 * ndof, 2 * ndof)
-        idx2 = slice(2 * ndof, 3 * ndof)
-
-        H0[idx0, idx0] = Ko + 2 * (K + Ksdt * accel)
-
-        K_aux = 1j * beta * speed
-        aux_C = 1j * speed
-        aux1 = 1 * K_aux + 1
-
-        H0[idx1, idx2] = np.conjugate(aux1) * Kn[:, :, 1] - 1 * aux_C * Cn[:, :, 1]
-        H0[idx2, idx1] = aux1 * Kn_s[:, :, 1] + 1 * aux_C * Cn_s[:, :, 1]
-
-        for n in range(1, self.noh + 1):
-            idx3 = slice((2 * n - 1) * ndof, 2 * n * ndof)
-            idx4 = slice(2 * n * ndof, (2 * n + 1) * ndof)
-
-            aux2 = -2 * n**2 * speed**2 + 2 * n * aux_C * alpha
-            aux3 = 2 * n * 1j * speed**2
-            aux4 = n * aux_C
-            aux5 = 2 * n * K_aux + 2
-            aux6 = 1 * n * K_aux + 1
-            H0[idx3, idx3] = (
-                aux2 * M
-                + aux3 * G
-                + aux4 * (2 * C + Co)
-                + aux5 * K
-                + 2 * (Ksdt * accel)
-                + aux6 * Ko
-            )
-
-            H0[idx4, idx4] = (
-                np.conjugate(aux2) * M
-                + np.conjugate(aux3) * G
-                + np.conjugate(aux4) * (2 * C + Co)
-                + np.conjugate(aux5) * K
-                + 2 * (Ksdt * accel)
-                + np.conjugate(aux6) * Ko
-            )
-
-            H0[idx3, idx0] = Kn[:, :, n - 1]
-            H0[idx4, idx0] = Kn_s[:, :, n - 1]
-            H0[idx0, idx3] = aux6 * Kn_s[:, :, n - 1] + aux4 * Cn_s[:, :, n - 1]
-            H0[idx0, idx4] = (
-                np.conjugate(aux6) * Kn[:, :, n - 1]
-                + np.conjugate(aux4) * Cn[:, :, n - 1]
-            )
-
-            if n < self.noh:
-                aux7 = 2 * self.noh - 2 * n + 2
-
-                for k in range(2, aux7):
-                    idx5 = slice((k - 1) * ndof, k * ndof)
-                    idx6 = slice((k - 1 + 2 * n) * ndof, (k + 2 * n) * ndof)
-                    aux8 = np.ceil((k + 2 * n - 1) / 2)
-                    if np.mod(k, 2) == 1:
-                        A = (-aux8 * K_aux + 1) * Kn[:, :, n - 1] - aux8 * aux_C * Cn[
-                            :, :, n - 1
-                        ]
-                    else:
-                        A = (aux8 * K_aux + 1) * Kn_s[
-                            :, :, n - 1
-                        ] + aux8 * aux_C * Cn_s[:, :, n - 1]
-
-                    H0[idx5, idx6] = A
-
-                aux9 = 2 * n + 1
-                for k in range(2 * self.noh + 1, aux9, -1):
-                    idx7 = slice((k - 1) * ndof, k * ndof)
-                    idx8 = slice((k - 1 - 2 * n) * ndof, (k - 2 * n) * ndof)
-                    aux10 = np.ceil((k - 2 * n - 1) / 2)
-                    if np.mod(k, 2) == 1:
-                        B = (-aux10 * K_aux + 1) * Kn_s[
-                            :, :, n - 1
-                        ] - aux10 * aux_C * Cn_s[:, :, n - 1]
-                    else:
-                        B = (+aux10 * K_aux + 1) * Kn[:, :, n - 1] + aux10 * aux_C * Cn[
-                            :, :, n - 1
-                        ]
-
-                    H0[idx7, idx8] = B
-
-        aux11 = 2 * self.noh - 1
-        for n in range(2 * self.noh, 2, -1):
-            aux12 = 2 * n - 1
-
-            if aux12 > 2 * self.noh:
-                aux12 = 2 * self.noh + 1
-
-            if aux11 < 2:
-                aux11 = 1
-            aux13 = 2 * n + 1
-
-            for k in range(aux12, aux11, -1):
-                idx9 = slice((k - 1) * ndof, k * ndof)
-                idx10 = slice((aux13 - k - 1) * ndof, (aux13 - k) * ndof)
-                aux14 = np.ceil((aux13 - k - 1) / 2)
-                if np.mod(k, 2) == 1:
-                    CC = (+aux14 * K_aux + 1) * Kn_s[
-                        :, :, n - 1
-                    ] + aux14 * aux_C * Cn_s[:, :, n - 1]
-                else:
-                    CC = (-aux14 * K_aux + 1) * Kn[:, :, n - 1] - aux14 * aux_C * Cn[
-                        :, :, n - 1
-                    ]
-                H0[idx9, idx10] = CC
-            aux11 = aux11 - 2
-
-        return H0
 
     def _solve_freq_response(self, H, F):
         ndof = self.rotor.ndof
@@ -473,3 +319,162 @@ class HarmonicBalance:
         y2dot = sum_y2dot
 
         return y, ydot, y2dot
+
+    @staticmethod
+    def _expand_Fourier(F, dt, fo, size):
+        row, N = F.shape
+        Fn = np.array(np.zeros((row, size)), dtype=complex)
+
+        b = int(np.floor(N / 2))
+        df = 1 / (N * dt)
+
+        A = fft(F)
+        X = A[:, :b]
+        X = X * 2 / N
+        Fo = 1 * np.real(X[:, 0])
+        an = 1 * np.real(X)
+        bn = -1 * np.imag(X)
+
+        for n in range(1, size + 1):
+            idx = int(n * fo / df)
+            Fn[:, n - 1] = an[:, idx] - 1j * bn[:, idx]
+
+        return Fo, Fn
+
+    @staticmethod
+    def _build_harmonic_balance_matrix(
+        ndof,
+        noh,
+        speed,
+        accel,
+        M,
+        K,
+        Ksdt,
+        C,
+        G,
+        Ko,
+        Kn,
+        Kn_s,
+        Co,
+        Cn,
+        Cn_s,
+        alpha,
+        beta,
+    ):
+        size = ndof * (2 * noh + 1)
+        H0 = np.zeros((size, size), dtype=complex)
+
+        idx0 = slice(0, ndof)
+        idx1 = slice(1 * ndof, 2 * ndof)
+        idx2 = slice(2 * ndof, 3 * ndof)
+
+        H0[idx0, idx0] = Ko + 2 * (K + Ksdt * accel)
+
+        K_aux = 1j * beta * speed
+        aux_C = 1j * speed
+        aux1 = 1 * K_aux + 1
+
+        H0[idx1, idx2] = np.conjugate(aux1) * Kn[:, :, 1] - 1 * aux_C * Cn[:, :, 1]
+        H0[idx2, idx1] = aux1 * Kn_s[:, :, 1] + 1 * aux_C * Cn_s[:, :, 1]
+
+        for n in range(1, noh + 1):
+            idx3 = slice((2 * n - 1) * ndof, 2 * n * ndof)
+            idx4 = slice(2 * n * ndof, (2 * n + 1) * ndof)
+
+            aux2 = -2 * n**2 * speed**2 + 2 * n * aux_C * alpha
+            aux3 = 2 * n * 1j * speed**2
+            aux4 = n * aux_C
+            aux5 = 2 * n * K_aux + 2
+            aux6 = 1 * n * K_aux + 1
+            H0[idx3, idx3] = (
+                aux2 * M
+                + aux3 * G
+                + aux4 * (2 * C + Co)
+                + aux5 * K
+                + 2 * (Ksdt * accel)
+                + aux6 * Ko
+            )
+
+            H0[idx4, idx4] = (
+                np.conjugate(aux2) * M
+                + np.conjugate(aux3) * G
+                + np.conjugate(aux4) * (2 * C + Co)
+                + np.conjugate(aux5) * K
+                + 2 * (Ksdt * accel)
+                + np.conjugate(aux6) * Ko
+            )
+
+            H0[idx3, idx0] = Kn[:, :, n - 1]
+            H0[idx4, idx0] = Kn_s[:, :, n - 1]
+            H0[idx0, idx3] = aux6 * Kn_s[:, :, n - 1] + aux4 * Cn_s[:, :, n - 1]
+            H0[idx0, idx4] = (
+                np.conjugate(aux6) * Kn[:, :, n - 1]
+                + np.conjugate(aux4) * Cn[:, :, n - 1]
+            )
+
+            if n < noh:
+                aux7 = 2 * noh - 2 * n + 2
+
+                for k in range(2, aux7):
+                    idx5 = slice((k - 1) * ndof, k * ndof)
+                    idx6 = slice((k - 1 + 2 * n) * ndof, (k + 2 * n) * ndof)
+                    aux8 = np.ceil((k + 2 * n - 1) / 2)
+
+                    if np.mod(k, 2) == 1:
+                        A_ = (-aux8 * K_aux + 1) * Kn[:, :, n - 1] - aux8 * aux_C * Cn[
+                            :, :, n - 1
+                        ]
+                    else:
+                        A_ = (aux8 * K_aux + 1) * Kn_s[
+                            :, :, n - 1
+                        ] + aux8 * aux_C * Cn_s[:, :, n - 1]
+
+                    H0[idx5, idx6] = A_
+
+                aux9 = 2 * n + 1
+                for k in range(2 * noh + 1, aux9, -1):
+                    idx7 = slice((k - 1) * ndof, k * ndof)
+                    idx8 = slice((k - 1 - 2 * n) * ndof, (k - 2 * n) * ndof)
+                    aux10 = np.ceil((k - 2 * n - 1) / 2)
+
+                    if np.mod(k, 2) == 1:
+                        B_ = (-aux10 * K_aux + 1) * Kn_s[
+                            :, :, n - 1
+                        ] - aux10 * aux_C * Cn_s[:, :, n - 1]
+                    else:
+                        B_ = (+aux10 * K_aux + 1) * Kn[
+                            :, :, n - 1
+                        ] + aux10 * aux_C * Cn[:, :, n - 1]
+
+                    H0[idx7, idx8] = B_
+
+        aux11 = 2 * noh - 1
+        for n in range(2 * noh, 2, -1):
+            aux12 = 2 * n - 1
+
+            if aux12 > 2 * noh:
+                aux12 = 2 * noh + 1
+
+            if aux11 < 2:
+                aux11 = 1
+            aux13 = 2 * n + 1
+
+            for k in range(aux12, aux11, -1):
+                idx9 = slice((k - 1) * ndof, k * ndof)
+                idx10 = slice((aux13 - k - 1) * ndof, (aux13 - k) * ndof)
+                aux14 = np.ceil((aux13 - k - 1) / 2)
+
+                if np.mod(k, 2) == 1:
+                    C_ = (+aux14 * K_aux + 1) * Kn_s[
+                        :, :, n - 1
+                    ] + aux14 * aux_C * Cn_s[:, :, n - 1]
+                else:
+                    C_ = (-aux14 * K_aux + 1) * Kn[:, :, n - 1] - aux14 * aux_C * Cn[
+                        :, :, n - 1
+                    ]
+
+                H0[idx9, idx10] = C_
+
+            aux11 = aux11 - 2
+
+        return H0
