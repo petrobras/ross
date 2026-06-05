52dbf16f3310e6535b2054d514bfeec9ce320149
Jessica Guarato <jguaratofs@gmail.com>
2026-06-05 14:22:40 -0300

    Consider numba in utils

--- ross/motors/utils.py
+++ ross/motors/utils.py
@@ -1,16 +1,20 @@
 import numpy as np
+from numba import njit
 
 
+@njit
 def phase_to_line(v):
     return v * np.sqrt(3)
 
 
+@njit
 def clarke_transform(a, b, c):
     alpha = 2 / 3 * (a - b / 2 - c / 2)
     beta = 2 / 3 * (b - c) * np.sqrt(3) / 2
     return alpha, beta
 
 
+@njit
 def park_transform(alpha, beta, theta):
     d = alpha * np.cos(theta) + beta * np.sin(theta)
     q = -alpha * np.sin(theta) + beta * np.cos(theta)
