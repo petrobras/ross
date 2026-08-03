71e115ad88c947a0d551ad0a85d2b6fd34dddfc7
raphaeltimbo <raphaelts@gmail.com>
2026-08-03 14:52:48 -0300

    Remove PlainJournalResults and TiltingPadResults
    Both classes served only the pre-rewrite solvers; PlainJournal and
    TiltingPad now delegate to FluidFilmBearingResults, whose plots are
    covered by test_fluid_film_bearing.

--- ross/tests/test_plot_results.py
+++ ross/tests/test_plot_results.py
@@ -5,18 +5,7 @@ from numpy.testing import assert_allclose
 
 from ross import Q_, Probe
 from ross.bearing_seal_element import BearingElement
-from ross.bearings.bearing_results import (
-    PlainJournalResults,
-    ThrustPadResults,
-    TiltingPadResults,
-)
-from ross.bearings.fluid_flow import fluid_flow_example
-from ross.bearings.fluid_flow_graphics import (
-    plot_eccentricity,
-    plot_pressure_surface,
-    plot_pressure_theta,
-    plot_pressure_z,
-)
+from ross.bearings.bearing_results import ThrustPadResults
 from ross.bearings.squeeze_film_damper import SqueezeFilmDamper
 from ross.disk_element import DiskElement
 from ross.materials import steel
@@ -196,45 +185,6 @@ def bearing_element():
 
 
 @pytest.fixture
-def tilting_pad_results():
-    nz, nx, n_pad = 4, 5, 2
-    pressure = np.linspace(0, 1e5, nz * nx * n_pad).reshape(nz, nx, n_pad)
-    temperature = np.linspace(40, 60, nz * nx * n_pad).reshape(nz, nx, n_pad)
-
-    return TiltingPadResults(
-        frequency=[100.0],
-        pressure_fields=[pressure],
-        temperature_fields=[temperature],
-        maxP_list=[pressure.max()],
-        maxT_list=[temperature.max()],
-        minH_list=[1e-5],
-        ecc_list=[0.3],
-        attitude_angle_list=[1.0],
-        psi_pad_list=[np.zeros(n_pad)],
-        force_x_total_list=[100.0],
-        force_y_total_list=[-200.0],
-        momen_rot_list=[np.zeros(n_pad)],
-        kxx=np.array([1e8]),
-        kxy=np.array([0.0]),
-        kyx=np.array([0.0]),
-        kyy=np.array([1e8]),
-        cxx=np.array([1e5]),
-        cxy=np.array([0.0]),
-        cyx=np.array([0.0]),
-        cyy=np.array([1e5]),
-        equilibrium_type="match_eccentricity",
-        n_pad=n_pad,
-        xtheta=np.linspace(-0.5, 0.5, nx),
-        xz=np.linspace(-0.5, 0.5, nz),
-        pivot_angle=np.array([0.3, 1.8]),
-        pad_axial_length=0.05,
-        nz=nz,
-        nx=nx,
-        optimization_history={},
-    )
-
-
-@pytest.fixture
 def thrust_pad_results():
     n_radial, n_theta = 3, 5
     d_radius, d_theta = 0.2, 0.2
@@ -267,60 +217,6 @@ def thrust_pad_results():
         pad_arc_length=pad_arc_length,
         optimization_history={},
     )
-
-
-@pytest.fixture
-def plain_journal_results():
-    elements_axial = 3
-    elements_circumferential = 4
-    n_pad = 2
-    circumferential_total = elements_circumferential * n_pad
-    pressure = np.linspace(1e4, 2e5, elements_axial * circumferential_total).reshape(
-        elements_axial, circumferential_total
-    )
-    temperature = np.linspace(40, 70, elements_axial * circumferential_total).reshape(
-        elements_axial, circumferential_total
-    )
-    theta = np.linspace(0, 2 * np.pi, circumferential_total)
-    z = np.linspace(0, 0.1, elements_axial)
-    theta_grid, z_grid = np.meshgrid(theta, z)
-    nondimensional_pressure = np.ones((elements_axial, elements_circumferential, n_pad))
-
-    return PlainJournalResults(
-        frequency=[100.0],
-        pressure_fields=[pressure],
-        temperature_fields=[temperature],
-        theta_grids=[theta_grid],
-        z_grids=[z_grid],
-        P_nondim_fields=[nondimensional_pressure],
-        kxx=np.array([1e9]),
-        kxy=np.array([0.0]),
-        kyx=np.array([0.0]),
-        kyy=np.array([1e9]),
-        cxx=np.array([1e5]),
-        cxy=np.array([0.0]),
-        cyx=np.array([0.0]),
-        cyy=np.array([1e5]),
-        fxs_load=0.0,
-        fys_load=-1000.0,
-        n_pad=n_pad,
-        betha_s_dg=176.0,
-        dtheta=0.1,
-        thetaF=np.array([np.pi, 2 * np.pi]),
-        elements_axial=elements_axial,
-        elements_circumferential=elements_circumferential,
-        equilibrium_pos_by_speed={100.0: [0.5, 0.1]},
-        opt_results={},
-        exec_times={},
-        optimization_history={},
-    )
-
-
-@pytest.fixture
-def fluid_flow():
-    bearing = fluid_flow_example()
-    bearing.calculate_pressure_matrix_analytical()
-    return bearing
 
 
 def _expected_frf_magnitude(freq_response, inp, out, amplitude_units="m/N"):
@@ -571,48 +467,6 @@ def test_bearing_element_plot_damping(bearing_element):
     assert_trace_allclose(fig.data[0], x=expected_x, y=expected_y)
 
 
-def test_tilting_pad_plot_pressure_3d(tilting_pad_results):
-    fig = tilting_pad_results.plot_pressure_3d()
-    assert_plotly_figure(fig)
-
-    pressure = tilting_pad_results.pressure_fields[0]
-    pad_index = int(np.argmax(pressure.max(axis=(0, 1))))
-    expected_z = 1e-6 * pressure[:, :, pad_index]
-    assert_surface_z_matches(fig.data[0], expected_z)
-
-
-def test_tilting_pad_plot_pressure_2d(tilting_pad_results):
-    fig = tilting_pad_results.plot_pressure_2d()
-    assert_plotly_figure(fig, min_traces=tilting_pad_results.n_pad)
-
-    pressure = tilting_pad_results.pressure_fields[0]
-    for pad_index in range(tilting_pad_results.n_pad):
-        assert_contour_z_matches(fig.data[pad_index], pressure[:, :, pad_index])
-
-
-def test_tilting_pad_plot_temperature_3d(tilting_pad_results):
-    fig = tilting_pad_results.plot_temperature_3d()
-    assert_plotly_figure(fig)
-
-    temperature = tilting_pad_results.temperature_fields[0]
-    pad_index = int(np.argmax(temperature.max(axis=(0, 1))))
-    assert_surface_z_matches(fig.data[0], temperature[:, :, pad_index])
-
-
-def test_tilting_pad_plot_results(tilting_pad_results):
-    figures = tilting_pad_results.plot_results()
-    assert {
-        "pressure_2d",
-        "pressure_3d",
-        "temperature_2d",
-        "temperature_3d",
-        "pressure_scatter",
-        "temperature_scatter",
-    }.issubset(figures.keys())
-    for fig in figures.values():
-        assert_plotly_figure(fig)
-
-
 def test_thrust_pad_plot_pressure_3d(thrust_pad_results):
     fig = thrust_pad_results.plot_pressure_3d()
     assert_plotly_figure(fig)
@@ -633,28 +487,6 @@ def test_thrust_pad_plot_temperature_3d(thrust_pad_results):
     assert_surface_z_matches(fig.data[0], thrust_pad_results.temperature_fields[0])
 
 
-def test_plain_journal_plot_pressure_2d(plain_journal_results):
-    fig = plain_journal_results.plot_pressure_2d()
-    assert_plotly_figure(fig)
-    assert_contour_z_matches(fig.data[0], plain_journal_results.pressure_fields[0])
-
-
-def test_plain_journal_plot_pressure_3d(plain_journal_results):
-    fig = plain_journal_results.plot_pressure_3d()
-    assert_plotly_figure(fig)
-    assert_surface_z_matches(fig.data[0], plain_journal_results.pressure_fields[0])
-
-
-def test_plain_journal_plot_bearing_representation(plain_journal_results):
-    fig = plain_journal_results.plot_bearing_representation()
-    assert_plotly_figure(fig)
-    groove = (360 / plain_journal_results.n_pad) - plain_journal_results.betha_s_dg
-    expected_values = [groove / 2, plain_journal_results.betha_s_dg, groove / 2] * (
-        plain_journal_results.n_pad
-    )
-    assert_allclose(fig.data[0].values, expected_values)
-
-
 def test_squeeze_film_damper_field_plots_not_available():
     bearing = SqueezeFilmDamper(
         n=0,
@@ -673,41 +505,3 @@ def test_squeeze_film_damper_field_plots_not_available():
 
     figures = bearing.plot_results()
     assert figures == {}
-
-
-def test_fluid_flow_plot_pressure_theta(fluid_flow):
-    z = int(fluid_flow.nz / 2)
-    fig = plot_pressure_theta(fluid_flow, z=z)
-    assert_plotly_figure(fig)
-    assert_trace_allclose(
-        fig.data[0],
-        x=fluid_flow.gama[z],
-        y=fluid_flow.p_mat_analytical[z],
-    )
-
-
-def test_fluid_flow_plot_pressure_z(fluid_flow):
-    theta = int(fluid_flow.ntheta / 2)
-    fig = plot_pressure_z(fluid_flow, theta=theta)
-    assert_plotly_figure(fig)
-    assert_trace_allclose(
-        fig.data[0],
-        x=fluid_flow.z_list,
-        y=fluid_flow.p_mat_analytical[:, theta],
-    )
-
-
-def test_fluid_flow_plot_pressure_surface():
-    bearing = fluid_flow_example()
-    bearing.calculate_pressure_matrix_numerical()
-    fig = plot_pressure_surface(bearing)
-    assert_plotly_figure(fig)
-    assert_surface_z_matches(fig.data[0], bearing.p_mat_numerical.T)
-
-
-def test_fluid_flow_plot_eccentricity(fluid_flow):
-    z = int(fluid_flow.nz / 2)
-    fig = plot_eccentricity(fluid_flow, z=z)
-    assert_plotly_figure(fig, min_traces=2)
-    assert len(fig.data[0].x) == fluid_flow.ntheta
-    assert len(fig.data[1].x) == fluid_flow.ntheta
